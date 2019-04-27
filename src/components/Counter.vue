2fe945f5f4be56ee974e431014d32d788f50ed77
rowaxl <rowaxl0@gmail.com>
2019-04-27 13:38:02 +0900

    use function in template

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,12 +1,16 @@
 <template>
     <div>
-        <button v-on:click="increment('x', 1)">Inc x</button>
-        <button v-on:click="increment('y', 1)">Inc y</button>
-        <p v-on:mousemove="updateCoordinate">
+        <button v-on:click="x += 1">Inc x</button>
+        <button v-on:click="y += 1">Inc y</button>
+        <p v-on:mousemove="(event) => {
+            x = event.clientX;
+            y = event.clientY;
+            }"
+        >
             Coordinate: {{ x }} / {{ y }}
             - <span v-on:mousemove.stop="">DEAD SPOT</span>
         </p>
-        <input type="text" v-on:keyup.enter="entered"></input>
+        <input type="text" v-on:keyup.enter="entered" />
     </div>
 </template>
 
@@ -20,16 +24,16 @@ export default {
         }
     },
     methods: {
-        increment: function (name, int) {
-            switch(name) {
-                case 'x':
-                    this.x += int;
-                    break;
-                case 'y':
-                    this.y += int;
-                    break;
-            }
-        },
+        // increment: function (name, int) {
+        //     switch(name) {
+        //         case 'x':
+        //             this.x += int;
+        //             break;
+        //         case 'y':
+        //             this.y += int;
+        //             break;
+        //     }
+        // },
         updateCoordinate: function(event) {
             this.x = event.clientX;
             this.y = event.clientY;
