57f2df42475a8249e0dd554835afdb770c78e72a
rowaxl <rowaxl0@gmail.com>
2019-04-27 14:04:52 +0900

    add watch pattern

--- src/components/Computed.vue
+++ src/components/Computed.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
-        <p>Result: {{ output }}</p>
+        <p>Counter: {{ computedCounter }} | {{ computedCounterTwo }}</p>
+        <p>Result: {{ result() }} | {{ output }}</p>
         <button v-on:click="++computedCounter">Click!</button>
+        <button v-on:click="++computedCounterTwo">Click!</button>
     </div>
 </template>
 
@@ -10,16 +12,31 @@ export default {
     name: 'Computed',
     data: function() {
         return {
-            computedCounter : 0
+            computedCounter: 0,
+            computedCounterTwo: 0
         }
     },
     computed: {
         output: function() {
+            // eslint-disable-next-line
+            console.log('computed runs');
             return this.computedCounter > 0 ? 'clicked!' : 'not clicked yet!'
+        }
+    },
+    watch: {
+        computedCounter: function(value) {
+            // eslint-disable-next-line
+            console.log('watch runs value:', value);
+            const vm = this;
+            setTimeout(() => {
+                vm.computedCounter = 0;
+            }, 2000);
         }
     },
     methods: {
         result: function() {
+            // eslint-disable-next-line
+            console.log('methods runs');
             return this.computedCounter > 0 ? 'clicked!' : 'not clicked yet!'
         }
     }
