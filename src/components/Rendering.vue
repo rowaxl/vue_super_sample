772a695a2c2f47166e82d6fa4508468581d3945d
rowaxl <rowaxl0@gmail.com>
2019-05-05 15:12:18 +0900

    using v-show

--- src/components/Rendering.vue
+++ src/components/Rendering.vue
@@ -3,6 +3,12 @@
         Switch by v-if
         <p v-if="show">Show</p>
         <p v-else-if="!show">Unshown</p>
+
+        <template v-if="show">
+            <h6>Inside template</h6>
+        </template>
+
+        <h3 v-show="show">V-Show is using display:none </h3>
         <button @click="show = !show">Switch</button>
     </div>
 </template>
