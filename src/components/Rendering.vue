2c652c6e5fda1706172d87be5de9a232f883cd5d
rowaxl <rowaxl0@gmail.com>
2019-05-07 14:22:28 +0900

    use v-for with template

--- src/components/Rendering.vue
+++ src/components/Rendering.vue
@@ -14,6 +14,10 @@
         <ul :style="{textAlign: 'left'}">
             <li v-for="(ingredient, i) in ingredients" v-bind:key="ingredient">{{ ingredient }}({{ i }})</li>
         </ul>
+        <template v-for="(ingredient, i) in ingredients">
+            <h2 v-bind:key="i">INGREDIENT: {{ ingredient }}</h2>
+            <p v-bind:key="i">INDEX: {{ i }}</p>
+        </template>
     </div>
 </template>
 
